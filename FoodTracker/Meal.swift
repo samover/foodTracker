41c4c6b5f8f50a89430ccf388d9d283dca9d6d02
samover <samuel.overloop@gmail.com>
2015-12-16 11:18:21 +0000

    update model to use NSCoding

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -8,12 +8,24 @@
 
 import UIKit
 
-class Meal {
+class Meal: NSObject, NSCoding {
+    
     // MARK: Properties
     
     var name: String
     var photo: UIImage?
     var rating: Int
+    
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveUrl = DocumentsDirectory.URLByAppendingPathComponent("meals")
     
     // MARK: Initialization
     
@@ -21,9 +33,26 @@ class Meal {
         self.name = name
         self.photo = photo
         self.rating = rating
+        super.init()
         
         if name.isEmpty || rating < 0 || rating > 5 {
             return nil
         }
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        self.init(name: name, photo: photo, rating: rating)
     }
 }
